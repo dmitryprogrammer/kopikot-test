94931eb01d50a06ac65e5f6469bc002b8f0ea2ce
dpischalka <dpischalka@gmail.com>
2018-03-27 15:53:09 +0400

    Работа над подгрузкой данных.

--- src/components/app/app.component.ctrl.js
+++ src/components/app/app.component.ctrl.js
@@ -1,6 +1,10 @@
 class AppComponentCtrl {
     constructor(catalogsList) {
+        this.clickMoreCounter = 0;
         this.catalogsListService = catalogsList;
+        this.catalogsList = [];
+
+        this.loadMoreBtnText = "Load more";
     }
 
     /** init component
@@ -10,13 +14,29 @@ class AppComponentCtrl {
     }
 
     /** getCatalogsList - метод, для получения списка записей
+     * @param {number} offset - начало отсчёта списка данных для получения
      * @returns {void} */
-    getCatalogsList() {
-        this.catalogsListService()
+    getCatalogsList(offset = 0) {
+        this.catalogsListService(offset)
             .then(catalogsList => {
-                this.catalogsList = catalogsList.data.items;
-                console.log(this.catalogsList);
+                this.catalogsList = [...this.catalogsList, ...catalogsList.data.items];
             });
+    }
+
+    /**
+     * метод для загрузки категорий реагирующий по клику на кнопку "load more"
+     * @param {Event} e - объект события DOM-элемента
+     * */
+    loadMore(e) {
+        e.preventDefault();
+        this.clickMoreCounter++;
+
+        if (this.clickMoreCounter < 10) {
+
+            this.getCatalogsList(10 * this.clickMoreCounter);
+        } else {
+            this.loadMoreBtnText = "You can not click more";
+        }
     }
 }
 
